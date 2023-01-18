ad9c158971e2fc46232911e4f46258ed19bc8d67
Cansın <ayurekl97@gmail.com>
2023-01-19 00:19:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const api = {
+    key: "a2405c56d6dda656bade64620b3673c5",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] =useState({});
+
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+       setWeather(result)
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hava Durumu</h1>
+      <input
+        type="text"
+        id="bar"
+        placeholder="Şehir gir"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type="button" onClick={searchPressed}>
+        Search
+      </button>
+      <div className="content">
+        <div className="city">{weather.name}</div>
+        <div className="temp">{weather.main.temp}</div>
+        <div className="temp">{weather.weather[0].main}</div>
+      
+      </div>
     </div>
   );
 }
